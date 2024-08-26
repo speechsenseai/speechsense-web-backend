dc2c2169c7f2316bb949bc77dc9def0e97af8279
kreison <alankainazovr@gmail.com>
2024-08-26 14:20:26 +0600

    update cors

--- src/main.ts
+++ src/main.ts
@@ -8,10 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  });
+  app.enableCors();
   app.setGlobalPrefix('api');
 
   const config = new DocumentBuilder()
